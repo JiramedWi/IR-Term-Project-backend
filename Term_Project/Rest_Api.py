64c3e2732ab73e34c73fdd848e11ab9ba59e57ca
Jiramed <jiramed_wi@cmu.ac.th>
2022-02-17 03:10:12 +0700

    RestApi omly ranking

--- Term_Project/Rest_Api.py
+++ Term_Project/Rest_Api.py
@@ -1,31 +1,62 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
 import string
-lyrics_data = pd.read_csv("./Term_Project/data/lyrics-data.csv")
-lyrics_data.dropna(inplace=True)
+
+import pandas as pd
+from flask import Flask
+from flask_restful import Api, Resource, abort, reqparse
+# from flask_cors import CORS
+from numpy import require
+
+import requireFunction as fucntion
+from bm25 import BM25
+
+lyrics_data = './Term_Project/assets/data.csv'
 
 app = Flask(__name__)
 api = Api(app)
+# CORS(app)
 
 
-class cleanLyricsData(Resource):
+# validation request
+def notFoundScoreType():
+    abort(404, message='Select the score rank first!')
+
+
+class Lyricsdata(Resource):
     def get(self):
-        lyrics = lyrics_data.drop(
-            lyrics_data[lyrics_data['Idiom'] != 'ENGLISH'].index)
-        lyrics = lyrics_data['Lyric']
-        cleanned_lyrics = lyrics.apply(lambda s: s.translate(
-            str.maketrans('', '', string.punctuation + u'\xa0')))
-        cleanned_lyrics = cleanned_lyrics.apply(lambda s: s.lower())
-        cleanned_lyrics = cleanned_lyrics.apply(lambda s: s.translate(
-            str.maketrans(string.whitespace, ' '*len(string.whitespace), '')))
-        cleanned_lyrics = cleanned_lyrics.drop_duplicates()
-        cleanned_lyrics = cleanned_lyrics.to_dict()
-        return {'lyrics': cleanned_lyrics}, 200
+        data = pd.read_csv(lyrics_data)
+        data = data.tail(1)
+        data = data.to_dict()
+        return {'data': data}, 200
 
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('query', required=True, type=str)
+        parser.add_argument('score', required=True, type=str)
+        args = parser.parse_args()
+        query = args['query']
+        query = [query]
+
+        if args['score'] == 'tf':
+            tf = fucntion.tf_ranking(query)
+        elif args['score'] == 'tf-idf':
+            tf = fucntion.tf_idf_ranking(query)
+        elif args['score'] == 'bm25':
+            tf = fucntion.bm25_ranking(query[0])
+        else:
+            notFoundScoreType()
+        return {'ranks :': tf + query}, 200
+        # return {'lyrics': "query: "+query + " , score: "+score}, 200
+
+# class Iula(Resource):
+#     def post(self):
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('query', required=True, type=str)
+#         args = parser.parse_args()
+#         return {}
 
 # api.com/
-api.add_resource(cleanLyricsData, '/')
+api.add_resource(Lyricsdata, '/query')
+# api.add_resource(Iula, '/iula')
 
 if __name__ == '__main__':
     app.run(debug=True)
